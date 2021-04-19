4629c6e30db3f903517afb38e3fa9c6f48e6ebe1
jimrothstein <jimrothstein@gmail.com>
2021-04-19 16:28:48 -0700

    updates

---
+++ rmd/0900_script_for_0220.R
@@ -0,0 +1,8 @@
+
+# rmd/0900_script_for_0220.R
+# 
+#  USAGE:   source this .R file from inside a chunk in .Rmd file
+#
+# Plain-old *.R file
+# 0220_.Rmd source this file, as a way to keep .Rmd nice and trim.
+print ("hello from file:  0900_script_for_0220.R")
